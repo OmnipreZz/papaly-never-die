0e26e0d1fb1865d0dd7f0aa496f7e7a066631aa7
philippe <huhmiel@free.fr>
2018-10-22 19:13:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let listLink = ["https://vuejs.org/v2/guide/","https://github.com/15Dkatz/vue-guides"]
+let app = new Vue({
+  el: '#app',
+  data: {
+    title: 'PapaVue',
+      link: null,
+      linkArray: listLink
+    },
+    methods: {
+        addLink(){
+            this.linkArray.push(this.link) 
+        },
+        removeLink: function (index) {
+          this.linkArray.splice(index, 1);
+        }
+    },
+})

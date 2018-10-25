d3466ff1d1a5a9021e9ccc409804a60bafef8e82
51m0n2063r <simon.roger31@gmail.com>
2018-10-25 16:37:24 +0200

    plein de truc

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-let listLink = ["https://vuejs.org/v2/guide/","https://github.com/15Dkatz/vue-guides"]
+let li = ["https://vuejs.org/v2/guide/","https://github.com/15Dkatz/vue-guides"]
 let app = new Vue({
   el: '#app',
   data: {
     title: 'PapaVue',
+      listCat: {},
       link: null,
-      linkArray: listLink
+      cat: null,
+      linkArray: li,
+      catError: false
     },
     methods: {
+        addCategorie(){
+          this.listCat[this.cat] = [this.link]
+          this.listCat[this.cat].shift()
+          console.table(this.listCat)
+        },
         addLink(){
-            this.linkArray.push(this.link) 
+        if(this.cat){
+          if(!this.listCat[this.cat]){
+            this.listCat[this.cat] = [this.link]
+          } else {
+            this.listCat[this.cat].push(this.link)
+          }
+        } else {
+          this.catError = true
+          //faire une alerte flash
+        }  
+          console.table(this.listCat)
         },
         removeLink: function (index) {
           this.linkArray.splice(index, 1);
         }
-    },
+    }
 })

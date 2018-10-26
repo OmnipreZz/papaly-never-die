c56a54244125ea57c3bcd2bba7a8b508d8bb497f
Geoffrey <geoffrey.frioli@gmail.com>
2018-10-26 11:48:35 +0200

    rdy to merge

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ let app = new Vue({
   el: '#app',
   data: {
     title: 'PapaVue',
-      link: null,
+      link: "toto",
       linkArray: listLink
     },
     methods: {
         addLink(){
-            this.linkArray.push(this.link) 
+            this.linkArray.push(this.link)
         },
         removeLink: function (index) {
           this.linkArray.splice(index, 1);
